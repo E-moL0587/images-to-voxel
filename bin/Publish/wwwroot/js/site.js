e54e7a236ea44b456c376bb27a9c9c24c2f4833f
E-moL0587 <kai.pika.025@outlook.com>
2024-06-25 20:31:12 +0900

    update

--- bin/Publish/wwwroot/js/site.js
+++ bin/Publish/wwwroot/js/site.js
@@ -1,10 +1,1 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-  const titleScreen = document.getElementById('titleScreen');
-  const menuScreen = document.getElementById('menuScreen');
-  const titleButton = document.getElementById('titleButton');
-
-  titleButton.addEventListener('click', function () {
-      titleScreen.classList.add('hidden');
-      menuScreen.classList.remove('hidden');
-  });
-});
+﻿
