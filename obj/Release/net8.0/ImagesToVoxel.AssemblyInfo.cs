a760b80253b1bb4b9e589cbd830f456515a817dc
E-moL0587 <kai.pika.025@outlook.com>
2024-06-26 09:36:01 +0900

    update

--- obj/Release/net8.0/ImagesToVoxel.AssemblyInfo.cs
+++ obj/Release/net8.0/ImagesToVoxel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImagesToVoxel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da71fc2931042f0909991b33e4c09d2e1167de88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe956c7dcbf26ad66eb5aeb882a17b9c501c82d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImagesToVoxel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImagesToVoxel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
