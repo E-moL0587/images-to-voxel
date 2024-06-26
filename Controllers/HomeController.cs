d4c2af12a9378e4a61bbe55c399cd60ca3bb3472
E-moL0587 <kai.pika.025@outlook.com>
2024-06-26 15:46:04 +0900

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,18 +3,15 @@ using System.Text.Json;
 
 namespace ImagesToVoxel.Controllers {
   public class HomeController : Controller {
-    private readonly string _frontImagePath;
-    private readonly string _sideImagePath;
-    private readonly string _topImagePath;
+    private readonly string _frontImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/front.png");
+    private readonly string _sideImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/side.png");
+    private readonly string _topImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/top.png");
     private readonly ImagesToPixels _imagesToPixels;
     private readonly PixelsToVoxel _pixelsToVoxel;
     private readonly MarchingCubes _marchingCubes;
     private readonly LaplacianSmoothing _laplacianSmoothing;
 
     public HomeController() {
-      _frontImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/front.png");
-      _sideImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/side.png");
-      _topImagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/top.png");
       _imagesToPixels = new ImagesToPixels();
       _pixelsToVoxel = new PixelsToVoxel();
       _marchingCubes = new MarchingCubes();
@@ -22,15 +19,7 @@ namespace ImagesToVoxel.Controllers {
     }
 
     public IActionResult Index() {
-      var frontBinaryData = _imagesToPixels.Pixel(_frontImagePath);
-      var sideBinaryData = _imagesToPixels.Pixel(_sideImagePath);
-      var topBinaryData = _imagesToPixels.Pixel(_topImagePath);
-
-      ViewData["FrontBinaryData"] = frontBinaryData;
-      ViewData["SideBinaryData"] = sideBinaryData;
-      ViewData["TopBinaryData"] = topBinaryData;
-
-      var (voxelData, _, _, _) = PrepareVoxel();
+      var (voxelData, frontBinaryData, sideBinaryData, topBinaryData) = PrepareVoxel();
       var meshData = _marchingCubes.Mesh(voxelData, 20, 20, 20);
       var smoothData = _laplacianSmoothing.Smooth(meshData);
 
@@ -38,7 +27,37 @@ namespace ImagesToVoxel.Controllers {
       ViewData["MeshData"] = JsonSerializer.Serialize(meshData);
       ViewData["SmoothData"] = JsonSerializer.Serialize(smoothData);
 
+      ViewData["FrontBinaryData"] = frontBinaryData;
+      ViewData["SideBinaryData"] = sideBinaryData;
+      ViewData["TopBinaryData"] = topBinaryData;
+
       return View();
+    }
+
+    [HttpPost]
+    public IActionResult Upload(IFormFile frontImage, IFormFile sideImage, IFormFile topImage) {
+      if (frontImage != null) {
+        using (var stream = new MemoryStream()) {
+          frontImage.CopyTo(stream);
+          System.IO.File.WriteAllBytes(_frontImagePath, stream.ToArray());
+        }
+      }
+
+      if (sideImage != null) {
+        using (var stream = new MemoryStream()) {
+          sideImage.CopyTo(stream);
+          System.IO.File.WriteAllBytes(_sideImagePath, stream.ToArray());
+        }
+      }
+
+      if (topImage != null) {
+        using (var stream = new MemoryStream()) {
+          topImage.CopyTo(stream);
+          System.IO.File.WriteAllBytes(_topImagePath, stream.ToArray());
+        }
+      }
+
+      return RedirectToAction("Index");
     }
 
     private (List<int[]> voxelData, string frontBinaryData, string sideBinaryData, string topBinaryData) PrepareVoxel() {
