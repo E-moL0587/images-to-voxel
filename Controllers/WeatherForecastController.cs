9ada6bdb41b7ac58a5ff7cf1424057b246f0db84
E-moL0587 <kai.pika.025@outlook.com>
2024-06-30 19:42:13 +0900

    update

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,60 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 
-namespace ImagesToVoxel.Controllers
-{
+namespace ImagesToVoxel.Controllers {
   [ApiController]
   [Route("[controller]")]
-  public class WeatherForecastController : ControllerBase
-  {
+  public class WeatherForecastController : ControllerBase {
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
+    public WeatherForecastController(ILogger<WeatherForecastController> logger) {
       _logger = logger;
     }
 
     [HttpPost("processtovoxel")]
-    public ActionResult<List<int[]>> ProcessToVoxel([FromBody] ImageData imageData)
-    {
+    public ActionResult<List<int[]>> ProcessToVoxel([FromBody] ImageData imageData) {
       var processor = new PixelsToVoxel();
       var voxelData = processor.Voxel(imageData.FrontData, imageData.SideData, imageData.TopData, imageData.Width);
       return Ok(voxelData);
     }
   }
 
-  public class ImageData
-  {
-    public string FrontData { get; set; }
-    public string SideData { get; set; }
-    public string TopData { get; set; }
+  public class ImageData {
+    public string? FrontData { get; set; }
+    public string? SideData { get; set; }
+    public string? TopData { get; set; }
     public int Width { get; set; }
   }
 
-  public class PixelsToVoxel
-  {
-    public List<int[]> Voxel(string frontData, string sideData, string topData, int width)
-    {
+  public class PixelsToVoxel {
+    public List<int[]> Voxel(string? frontData, string? sideData, string? topData, int width) {
       var voxelData = new List<int[]>();
-
-      for (int y = 0; y < width; y++)
-      {
-        for (int x = 0; x < width; x++)
-        {
-          for (int z = 0; z < width; z++)
-          {
+      for (int y = 0; y < width; y++) {
+        for (int x = 0; x < width; x++) {
+          for (int z = 0; z < width; z++) {
             int frontIndex = y * width + x;
             int sideIndex = y * width + z;
             int topIndex = x * width + z;
-
-            if (frontData[frontIndex] == '0' && sideData[sideIndex] == '0' && topData[topIndex] == '0')
-            {
+            if (frontData?[frontIndex] == '0' && sideData?[sideIndex] == '0' && topData?[topIndex] == '0') {
               voxelData.Add(new int[] { x, y, z });
             }
           }
         }
       }
-
       return voxelData;
     }
   }
