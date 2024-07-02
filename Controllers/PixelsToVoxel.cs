8f2111d4cb8a15d74d2ea61a2404f9c2693987d4
E-moL0587 <kai.pika.025@outlook.com>
2024-07-02 14:57:05 +0900

    update

--- Controllers/PixelsToVoxel.cs
+++ Controllers/PixelsToVoxel.cs
@@ -2,6 +2,8 @@ namespace ImagesToVoxel.Controllers {
   public class PixelsToVoxel {
     public List<int[]> Voxel(string frontData, string sideData, string topData, int size) {
       var voxelData = new List<int[]>();
+      frontData = FlipXAxis(frontData, size);
+      topData = RotateAndFlipXAxis(topData, size);
 
       for (int y = 0; y < size; y++) {
         for (int x = 0; x < size; x++) {
@@ -10,16 +12,34 @@ namespace ImagesToVoxel.Controllers {
             int sideIndex = y * size + z;
             int topIndex = x * size + z;
 
-            if (frontIndex < frontData.Length && sideIndex < sideData.Length && topIndex < topData.Length) {
-              if (frontData[frontIndex] == '0' && sideData[sideIndex] == '0' && topData[topIndex] == '0') {
-                voxelData.Add(new int[] { x, y, z });
-              }
+            if (frontData[frontIndex] == '0' && sideData[sideIndex] == '0' && topData[topIndex] == '0') {
+              voxelData.Add(new int[] { x, y, z });
             }
           }
         }
       }
 
       return voxelData;
+    }
+
+    private string FlipXAxis(string data, int size) {
+      var flipped = new char[data.Length];
+      for (int y = 0; y < size; y++) {
+        for (int x = 0; x < size; x++) {
+          flipped[y * size + x] = data[y * size + (size - x - 1)];
+        }
+      }
+      return new string(flipped);
+    }
+
+    private string RotateAndFlipXAxis(string data, int size) {
+      var rotatedFlipped = new char[data.Length];
+      for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+          rotatedFlipped[j * size + (size - i - 1)] = data[i * size + j];
+        }
+      }
+      return new string(rotatedFlipped);
     }
   }
 }
