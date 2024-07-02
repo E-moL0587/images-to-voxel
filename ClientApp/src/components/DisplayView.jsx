8ed75b45338882a7482720774acb60d04d224ce1
E-moL0587 <kai.pika.025@outlook.com>
2024-07-02 18:35:31 +0900

    update

--- ClientApp/src/components/DisplayView.jsx
+++ ClientApp/src/components/DisplayView.jsx
@@ -11,74 +11,48 @@ export class DisplayView extends Component {
 
   getSurfaceVoxels(data) {
     if (!data) return [];
-    
-    const isSurfaceVoxel = (x, y, z, data) => {
-      const directions = [
-        [1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]
-      ];
-      return directions.some(([dx, dy, dz]) => !data.some(([nx, ny, nz]) => nx === x + dx && ny === y + dy && nz === z + dz));
-    };
+    const directions = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]];
+    return data.filter(([x, y, z]) => directions.some(([dx, dy, dz]) => !data.some(([nx, ny, nz]) => nx === x + dx && ny === y + dy && nz === z + dz)));
+  }
 
-    return data.filter(([x, y, z]) => isSurfaceVoxel(x, y, z, data));
+  computeBoundingBox(data) {
+    const box = new THREE.Box3();
+    if (data) data.forEach(([x, y, z]) => box.expandByPoint(new THREE.Vector3(x, y, z)));
+    return box;
   }
 
   VoxelScene = ({ data, color }) => {
     const surfaceVoxels = this.getSurfaceVoxels(data);
-
-    return (
-      <group>
-        {surfaceVoxels.map(([x, y, z], i) => (
-          <mesh key={i} position={[-x, -y, -z]}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshPhongMaterial color={color} />
-          </mesh>
-        ))}
-      </group>
-    );
+    const center = this.computeBoundingBox(surfaceVoxels).getCenter(new THREE.Vector3());
+    return <group>{surfaceVoxels.map(([x, y, z], i) => <mesh key={i} position={[-x + center.x, -y + center.y, -z + center.z]}><boxGeometry args={[1, 1, 1]} /><meshPhongMaterial color={color} /></mesh>)}</group>;
   };
 
   MeshScene = ({ data, color }) => {
     if (!data) return null;
-
+    const center = this.computeBoundingBox(data).getCenter(new THREE.Vector3());
+    const vertices = new Float32Array(data.map(([x, y, z]) => [-x + center.x, -y + center.y, -z + center.z]).flat());
     const geometry = new THREE.BufferGeometry();
-    const vertices = new Float32Array(data.flat());
     geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
     geometry.computeVertexNormals();
-
-    return (
-      <group>
-        <mesh geometry={geometry}>
-          <meshPhongMaterial color={color} />
-        </mesh>
-      </group>
-    );
+    return <mesh geometry={geometry}><meshPhongMaterial color={color} /></mesh>;
   };
 
-  SceneWrapper = ({ displayType, voxelData, meshData, smoothData, color }) => {
-    return (
-      <>
-        {displayType === 'voxel' && <this.VoxelScene data={voxelData} color={color} />}
-        {displayType === 'mesh' && <this.MeshScene data={meshData} color={color} />}
-        {displayType === 'smooth' && <this.MeshScene data={smoothData} color={color} />}
-      </>
-    );
-  };
+  SceneWrapper = ({ displayType, voxelData, meshData, smoothData, color }) => (
+    <>
+      {displayType === 'voxel' && <this.VoxelScene data={voxelData} color={color} />}
+      {displayType === 'mesh' && <this.MeshScene data={meshData} color={color} />}
+      {displayType === 'smooth' && <this.MeshScene data={smoothData} color={color} />}
+    </>
+  );
 
   render() {
     const { displayType, voxelData, meshData, smoothData, color } = this.props;
-
     return (
-      <Canvas ref={this.canvasRef} style={{ width: '100vw', height: '100vw' }}>
+      <Canvas ref={this.canvasRef} style={{ width: '100vw', height: '100vw', background: '#000' }}>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 10, 7.5]} intensity={1} />
+        <directionalLight position={[10, 10, 10]} intensity={1.5} />
         <OrbitControls />
-        <this.SceneWrapper
-          displayType={displayType}
-          voxelData={voxelData}
-          meshData={meshData}
-          smoothData={smoothData}
-          color={color}
-        />
+        <this.SceneWrapper displayType={displayType} voxelData={voxelData} meshData={meshData} smoothData={smoothData} color={color} />
       </Canvas>
     );
   }
