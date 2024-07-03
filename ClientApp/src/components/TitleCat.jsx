a6bdfdd340091edf9de7781bd8f5a21566999242
E-moL0587 <kai.pika.025@outlook.com>
2024-07-04 04:19:12 +0900

    update

---
+++ ClientApp/src/components/TitleCat.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class TitleCat extends Component {
+  render() {
+    return (
+      <div>
+        Cat
+      </div>
+    );
+  }
+}
--- ClientApp/src/components/TitleView.jsx
+++ ClientApp/src/components/TitleView.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { TitleCat } from './TitleCat';
 
 export class TitleView extends Component {
   render() {
     return (
       <div>
         <h1>Title View</h1>
+        <TitleCat />
         <button onClick={this.props.switchToMain}>Go to Main View</button>
         <button onClick={this.props.switchToExplan}>Go to Explan View</button>
       </div>
