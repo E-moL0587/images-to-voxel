c62a69c3b2c8175d257f84bed20198efec625b55
E-moL0587 <kai.pika.025@outlook.com>
2024-07-03 10:10:40 +0900

    update

--- ClientApp/src/components/DisplayView.jsx
+++ ClientApp/src/components/DisplayView.jsx
@@ -1,41 +1,51 @@
-import React, { useEffect, useRef } from 'react';
+import React, { Component, createRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter';
 
-export const DisplayView = ({ displayType, voxelData, meshData, smoothData, color }) => {
-  const materialRef = useRef(new THREE.MeshPhongMaterial({ side: THREE.DoubleSide }));
-  materialRef.current.color.set(color);
+export class DisplayView extends Component {
+  constructor(props) {
+    super(props);
+    this.materialRef = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide });
+    this.sceneRef = createRef();
+  }
 
-  useEffect(() => {
-    materialRef.current.color.set(color);
-  }, [color]);
+  componentDidMount() {
+    this.materialRef.color.set(this.props.color);
+  }
 
-  const addVoxels = (data) => {
+  componentDidUpdate(prevProps) {
+    if (prevProps.color !== this.props.color) {
+      this.materialRef.color.set(this.props.color);
+    }
+  }
+
+  addVoxels = (data) => {
     if (!data) return [];
-    const surfaceVoxels = getSurfaceVoxels(data);
-    const center = computeBoundingBox(surfaceVoxels).getCenter(new THREE.Vector3());
+    const surfaceVoxels = this.getSurfaceVoxels(data);
+    const center = this.computeBoundingBox(surfaceVoxels).getCenter(new THREE.Vector3());
     return surfaceVoxels.map(([x, y, z], index) => (
-      <mesh key={index} position={[-x + center.x, -y + center.y, -z + center.z]} material={materialRef.current}>
+      <mesh key={index} position={[-x + center.x, -y + center.y, -z + center.z]} material={this.materialRef}>
         <boxGeometry args={[1, 1, 1]} />
       </mesh>
     ));
   };
 
-  const addMesh = (data) => {
+  addMesh = (data) => {
     if (!data) return null;
-    const center = computeBoundingBox(data).getCenter(new THREE.Vector3());
+    const center = this.computeBoundingBox(data).getCenter(new THREE.Vector3());
     const vertices = new Float32Array(data.flatMap(([x, y, z]) => [-x + center.x, -y + center.y, -z + center.z]));
     const geometry = new THREE.BufferGeometry();
     geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
     geometry.computeVertexNormals();
     return (
-      <mesh geometry={geometry} material={materialRef.current} />
+      <mesh geometry={geometry} material={this.materialRef} />
     );
   };
 
-  const getSurfaceVoxels = (data) => {
-    const directions = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]];
+  getSurfaceVoxels = (data) => {
+    const directions = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [-1, 0], [0, 0, 1], [0, 0, -1]];
     return data.filter(([x, y, z]) =>
       directions.some(([dx, dy, dz]) =>
         !data.some(([nx, ny, nz]) =>
@@ -45,21 +55,53 @@ export const DisplayView = ({ displayType, voxelData, meshData, smoothData, colo
     );
   };
 
-  const computeBoundingBox = (data) => {
+  computeBoundingBox = (data) => {
     const box = new THREE.Box3();
     if (data) data.forEach(([x, y, z]) => box.expandByPoint(new THREE.Vector3(x, y, z)));
     return box;
   };
 
-  return (
-    <Canvas style={{ width: '100vw', height: '100vw' }}>
-      <ambientLight intensity={1.0} />
-      <directionalLight position={[10, 5, 10]} intensity={2.0} />
-      <directionalLight position={[-10, 5, 10]} intensity={2.0} />
-      <directionalLight position={[0, 5, -10]} intensity={2.0} />
-      <OrbitControls />
-      {displayType === 'voxel' && addVoxels(voxelData)}
-      {(displayType === 'mesh' || displayType === 'smooth') && addMesh(displayType === 'mesh' ? meshData : smoothData)}
-    </Canvas>
-  );
-};
+  exportGLB = () => {
+    if (this.sceneRef.current) {
+      if (window.confirm('Do you want to export the 3D model?')) {
+        const exporter = new GLTFExporter();
+        const options = { binary: true };
+        exporter.parse(this.sceneRef.current, (result) => {
+          if (result instanceof ArrayBuffer) {
+            const blob = new Blob([result], { type: 'model/gltf-binary' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'model.glb';
+            a.click();
+            URL.revokeObjectURL(url);
+          } else {
+            console.error('GLTF Export Error: Result is not an ArrayBuffer.');
+          }
+        }, (error) => {
+          console.error('GLTF Export Error:', error);
+        }, options);
+      }
+    } else {
+      console.error('Scene reference is not available.');
+    }
+  };
+
+  render() {
+    const { displayType, voxelData, meshData, smoothData } = this.props;
+    return (
+      <>
+        <Canvas style={{ width: '100vw', height: '100vw' }} ref={this.sceneRef}>
+          <ambientLight intensity={1.0} />
+          <directionalLight position={[10, 5, 10]} intensity={2.0} />
+          <directionalLight position={[-10, 5, 10]} intensity={2.0} />
+          <directionalLight position={[0, 5, -10]} intensity={2.0} />
+          <OrbitControls />
+          {displayType === 'voxel' && this.addVoxels(voxelData)}
+          {(displayType === 'mesh' || displayType === 'smooth') && this.addMesh(displayType === 'mesh' ? meshData : smoothData)}
+        </Canvas>
+        <button onClick={this.exportGLB} style={{ position: 'absolute', top: '10px', right: '10px' }}>Export GLB</button>
+      </>
+    );
+  }
+}
