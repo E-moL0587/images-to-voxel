1df7b0c3bf669b5975c67488951e85406711e571
E-moL0587 <kai.pika.025@outlook.com>
2024-07-06 00:20:56 +0900

    update

--- ClientApp/src/components/TitleView.jsx
+++ ClientApp/src/components/TitleView.jsx
@@ -36,13 +36,13 @@ export class TitleView extends Component {
 
         <style>
           {`
-            .title-container { position: relative; height: 100vh; display: flex; flex-direction: column; justify-content: space-between; opacity: 1; transition: opacity 2s ease; }
+            .title-container { position: relative; height: 100vh; display: grid; place-items: center; opacity: 1; transition: opacity 2s ease; }
             .fade-in { opacity: 0; }
             .fade-out { opacity: 0; }
             .background { position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: -1; }
-            .title { position: absolute; font-size: 3em; font-weight: bold; top: 20%; left: 50%; transform: translate(-50%, -50%); color: #f0f0f0; }
-            .subtitle { position: absolute; font-size: 1.5em; font-weight: bold; bottom: 10%; left: 50%; transform: translate(-50%, -50%); color: #f0f0f0; }
-            .start-button { position: absolute; bottom: 30%; left: 50%; transform: translate(-50%, -50%); }
+            .title { position: absolute; font-size: 2.5em; font-weight: bold; top: 20%; color: #f0f0f0; }
+            .subtitle { position: absolute; font-size: 1.0em; bottom: 20%; color: #f0f0f0; }
+            .start-button { position: absolute; bottom: 30%; }
           `}
         </style>
       </>
